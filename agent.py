62c17a8580d50cf383a3742674879b1705472293
bprathamesh20 <bprathamesh2003@gmail.com>
2024-10-06 01:39:23 +0530

    initial commit

---
+++ agent.py
@@ -0,0 +1,68 @@
+import logging
+
+from dotenv import load_dotenv
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    JobProcess,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.pipeline import VoicePipelineAgent
+from livekit.plugins import openai, deepgram, silero
+
+
+load_dotenv(dotenv_path=".env.local")
+logger = logging.getLogger("voice-agent")
+
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "Your name is alex. You are a interview agent that interview computer science students. "
+            "You are helpful, polite, and eager to help the user. You will ask the user questions about computer science "
+            "when the user joins the room. You begin the interview casually by asking them casual questions like how are you  "
+            "After the user answers the question, you give them feedback on their answer about how could they improve their answer. "
+            "you will ask 5 questions to the user and then end the interview."
+            "At the end of the interview, you will give the user a final feedback on their interview . "
+        ),
+    )
+
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    # Wait for the first participant to connect
+    participant = await ctx.wait_for_participant()
+    logger.info(f"starting voice assistant for participant {participant.identity}")
+
+    # This project is configured to use Deepgram STT, OpenAI LLM and TTS plugins
+    # Other great providers exist like Cartesia and ElevenLabs
+    # Learn more and pick the best one for your app:
+    # https://docs.livekit.io/agents/plugins
+    assistant = VoicePipelineAgent(
+        vad=ctx.proc.userdata["vad"],
+        stt=deepgram.STT(),
+        llm=openai.LLM(model="gpt-4o-mini"),
+        tts=openai.TTS(),
+        chat_ctx=initial_ctx,
+    )
+
+    assistant.start(ctx.room, participant)
+
+    # The agent should be polite and greet the user when it joins :)
+    await assistant.say("Hey There, I am alex and I am here conduct your interview., shall we begin?")
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+            prewarm_fnc=prewarm,
+        ),
+    )
